340d141db281b91728a2acb19ee046141904d2cf
fen <fen1211@gmail.com>
2013-10-11 22:41:15 +0800

    改进页脚

--- admin/copyright.php
+++ admin/copyright.php
@@ -1,10 +1,12 @@
 <?php if(!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <div class="typecho-foot">
-    <div class="copyright"><?php _e('基于 <a href="http://typecho.org">%s</a> %s %s 构建', $options->software, $prefixVersion, $suffixVersion); ?></div>
+    <div class="copyright">
+        <?php _e('基于 <a href="http://typecho.org">%s</a> 构建, 版本 %s # %s ', $options->software, $prefixVersion, $suffixVersion); ?>
+    </div>
     <nav class="resource">
-        <a href="http://docs.typecho.org"><?php _e('文档'); ?></a>
-        <a href="http://forum.typecho.org"><?php _e('支持论坛'); ?></a>
-        <a href="http://code.google.com/p/typecho/issues/entry"><?php _e('报告错误'); ?></a>
-        <a href="http://extends.typecho.org"><?php _e('其他资源'); ?></a>
+        <a href="http://docs.typecho.org"><?php _e('帮助文档'); ?></a> &bull;
+        <a href="http://forum.typecho.org"><?php _e('支持论坛'); ?></a> &bull;
+        <a href="https://github.com/typecho/typecho-replica/issues"><?php _e('报告错误'); ?></a> &bull;
+        <a href="http://extends.typecho.org"><?php _e('资源下载'); ?></a>
     </nav>
 </div>
