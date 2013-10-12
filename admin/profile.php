0e86a91c9d1153bb7872531d0ea04c14df217496
fen <fen1211@gmail.com>
2013-10-12 21:32:01 +0800

    调整布局

--- admin/profile.php
+++ admin/profile.php
@@ -10,12 +10,30 @@ $stat = Typecho_Widget::widget('Widget_Stat');
     <div class="body container">
         <?php include 'page-title.php'; ?>
         <div class="col-group typecho-page-main">
-            <div class="col-mb-12 col-tb-7 typecho-content-panel">
-                <h4>
-                <?php echo '<img class="avatar" src="http://www.gravatar.com/avatar/' . md5($user->mail) . '?s=50&r=X' .
-                '&d=" alt="' . $user->screenName . '" width="50" height="50" />'; ?>
-                <?php $user->name(); ?><cite>(<?php $user->screenName(); ?>)</cite>
-                </h4>
+            <div class="col-mb-12 col-tb-9 typecho-content-panel">
+                <fieldset>
+                    <legend><?php _e('个人资料'); ?></legend>
+                    <?php Typecho_Widget::widget('Widget_Users_Profile')->profileForm()->render(); ?>
+                </fieldset>
+
+                <?php if($user->pass('contributor', true)): ?>
+                <fieldset id="writing-option">
+                    <legend><?php _e('撰写设置'); ?></legend>
+                    <?php Typecho_Widget::widget('Widget_Users_Profile')->optionsForm()->render(); ?>
+                </fieldset>
+                <?php endif; ?>
+
+                <fieldset id="change-password">
+                    <legend><?php _e('密码修改'); ?></legend>
+                    <?php Typecho_Widget::widget('Widget_Users_Profile')->personalFormList(); ?>
+                    <?php Typecho_Widget::widget('Widget_Users_Profile')->passwordForm()->render(); ?>
+                </fieldset>
+                
+            </div>
+            <div class="col-tb-3">
+                <?php echo '<img class="avatar" src="http://www.gravatar.com/avatar/' . md5($user->mail) . '?s=128&r=X' .
+                '&d=" alt="' . $user->screenName . '" />'; ?>
+                <h2><?php $user->screenName(); ?><br><small><?php $user->name(); ?></small></h2>
                 <p><?php _e('目前有 <em>%s</em> 篇 Blog,并有 <em>%s</em> 条关于你的评论在已设定的 <em>%s</em> 个分类中.', 
                 $stat->myPublishedPostsNum, $stat->myPublishedCommentsNum, $stat->categoriesNum); ?></p>
                 <p><?php
@@ -23,16 +41,6 @@ $stat = Typecho_Widget::widget('Widget_Stat');
                     _e('最后登录: %s', Typecho_I18n::dateWord($user->logged  + $options->timezone, $options->gmtTime + $options->timezone));
                 }
                 ?></p>
-                <?php if($user->pass('contributor', true)): ?>
-                <h3 id="writing-option"><?php _e('撰写设置'); ?></h3>
-                <?php Typecho_Widget::widget('Widget_Users_Profile')->optionsForm()->render(); ?>
-                <?php endif; ?>
-                <?php Typecho_Widget::widget('Widget_Users_Profile')->personalFormList(); ?>
-                <h3 id="change-password"><?php _e('设置密码'); ?></h3>
-                <?php Typecho_Widget::widget('Widget_Users_Profile')->passwordForm()->render(); ?>
-            </div>
-            <div class="col-mb-12 col-tb-5 typecho-mini-panel typecho-radius-topleft typecho-radius-topright typecho-radius-bottomleft typecho-radius-bottomright">
-                <?php Typecho_Widget::widget('Widget_Users_Profile')->profileForm()->render(); ?>
             </div>
         </div>
     </div>
