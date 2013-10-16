ee723e3101ed7a40598fa9df050efe9cdf9c5b5c
fen <fen1211@gmail.com>
2013-10-16 21:02:52 +0800

    改进导航菜单

--- admin/menu.php
+++ admin/menu.php
@@ -1,13 +1,13 @@
 <?php if(!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <div class="typecho-head-nav clearfix">
-    <a class="site-name" href="<?php $options->siteUrl(); ?>"><?php $options->title(); ?></a>
     <nav id="typecho-nav-list">
         <?php $menu->output(); ?>
     </nav>
     <div class="operate">
         <!-- <?php Typecho_Plugin::factory('admin/menu.php')->navBar(); _e('欢迎'); ?>, -->
         <a href="<?php $options->adminUrl('profile.php'); ?>" class="author"><?php $user->screenName(); ?></a><!--
-        --><a class="exit" href="<?php $options->logoutUrl(); ?>"><?php _e('登出'); ?></a>
+        --><a class="exit" href="<?php $options->logoutUrl(); ?>"><?php _e('登出'); ?></a><!--
+        --><a href="<?php $options->siteUrl(); ?>">网站</a>
     </div>
 </div>
 
--- admin/profile.php
+++ admin/profile.php
@@ -11,8 +11,8 @@ $stat = Typecho_Widget::widget('Widget_Stat');
         <?php include 'page-title.php'; ?>
         <div class="col-group typecho-page-main">
             <div class="col-mb-12 col-tb-3">
-                <p><?php echo '<img class="avatar" src="http://www.gravatar.com/avatar/' . md5($user->mail) . '?s=128&r=X' .
-                '&d=" alt="' . $user->screenName . '" />'; ?></p>
+                <p><a href="http://gravatar.com/emails/" title="<?php _e('在 Gravatar 上修改头像'); ?>"><?php echo '<img class="avatar" src="http://www.gravatar.com/avatar/' . md5($user->mail) . '?s=220&r=X' .
+                '&d=" alt="' . $user->screenName . '" />'; ?></a></p>
                 <h2><?php $user->screenName(); ?><br><small><?php $user->name(); ?></small></h2>
                 <p><?php _e('目前有 <em>%s</em> 篇 Blog,并有 <em>%s</em> 条关于你的评论在已设定的 <em>%s</em> 个分类中.', 
                 $stat->myPublishedPostsNum, $stat->myPublishedCommentsNum, $stat->categoriesNum); ?></p>
