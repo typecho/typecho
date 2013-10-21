e9793a8cdccae81ca0867eeb8733f7b24e35a7a8
fen <fen1211@gmail.com>
2013-10-21 23:42:53 +0800

    修复旧模板几个多年的bug

--- usr/themes/default/header.php
+++ usr/themes/default/header.php
@@ -13,28 +13,32 @@
 
 <body>
 <div id="header" class="container_16 clearfix">
-	<form id="search" method="post" action="/">
-		<div><input type="text" name="s" class="text" size="20" /> <input type="submit" class="submit" value="<?php _e('搜索'); ?>" /></div>
-    </form>
-	<div id="logo">
-	    <h1><a href="<?php $this->options->siteUrl(); ?>">
-        <?php if ($this->options->logoUrl): ?>
-        <img height="60" src="<?php $this->options->logoUrl() ?>" alt="<?php $this->options->title() ?>" />
-        <?php endif; ?>
-        <?php $this->options->title() ?>
-        </a></h1>
-	    <p class="description"><?php $this->options->description() ?></p>
+    <div class="grid_14">
+    	<form id="search" method="post" action="/">
+    		<div><input type="text" name="s" class="text" size="20" /> <input type="submit" class="submit" value="<?php _e('搜索'); ?>" /></div>
+        </form>
+    	<div id="logo">
+    	    <h1><a href="<?php $this->options->siteUrl(); ?>">
+            <?php if ($this->options->logoUrl): ?>
+            <img height="60" src="<?php $this->options->logoUrl() ?>" alt="<?php $this->options->title() ?>" />
+            <?php endif; ?>
+            <?php $this->options->title() ?>
+            </a></h1>
+    	    <p class="description"><?php $this->options->description() ?></p>
+        </div>
     </div>
 </div><!-- end #header -->
 
 <div id="nav_box" class="clearfix">
-<ul class="container_16 clearfix" id="nav_menu">
-    <li<?php if($this->is('index')): ?> class="current"<?php endif; ?>><a href="<?php $this->options->siteUrl(); ?>"><?php _e('首页'); ?></a></li>
-    <?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
-    <?php while($pages->next()): ?>
-    <li<?php if($this->is('page', $pages->slug)): ?> class="current"<?php endif; ?>><a href="<?php $pages->permalink(); ?>" title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a></li>
-    <?php endwhile; ?>
-</ul>
+    <div class="container_16">
+        <ul class="grid_14 clearfix" id="nav_menu">
+            <li<?php if($this->is('index')): ?> class="current"<?php endif; ?>><a href="<?php $this->options->siteUrl(); ?>"><?php _e('首页'); ?></a></li>
+            <?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
+            <?php while($pages->next()): ?>
+            <li<?php if($this->is('page', $pages->slug)): ?> class="current"<?php endif; ?>><a href="<?php $pages->permalink(); ?>" title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a></li>
+            <?php endwhile; ?>
+        </ul>
+    </div>
 </div>
 
 <div class="container_16 clearfix">
