c617578f485522283ba139bf43c091ddd400995d
fen <fen1211@gmail.com>
2013-10-22 14:39:18 +0800

    修改默认模板导航菜单

--- usr/themes/replica/archive.php
+++ usr/themes/replica/archive.php
@@ -1,27 +1,28 @@
 <?php $this->need('header.php'); ?>
 
-    <div class="col-mb-12 col-tb-8" id="content">
-    <?php if ($this->have()): ?>
-	<?php while($this->next()): ?>
-        <div class="post">
-			<h2 class="entry-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
-			<p class="entry-data">
-				<span><?php _e('作者：'); ?><?php $this->author(); ?></span>
-				<span><?php _e('发布时间：'); ?><?php $this->date('F j, Y'); ?></span>
-				<?php _e('分类：'); ?><?php $this->category(','); ?>
-			</p>
-			<?php $this->content('阅读剩余部分...'); ?>
-		</div>
-	<?php endwhile; ?>
-    <?php else: ?>
-        <div class="post">
-            <h2 class="entry-title"><?php _e('没有找到内容'); ?></h2>
-        </div>
-    <?php endif; ?>
+    <div class="col-mb-12 col-8" id="main">
+        <?php if ($this->have()): ?>
+    	<?php while($this->next()): ?>
+            <article class="post">
+    			<h2 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
+    			<div class="post-meta">
+    				<span><?php _e('作者：'); ?><?php $this->author(); ?></span>
+    				<span><?php _e('发布时间：'); ?><?php $this->date('F j, Y'); ?></span>
+    				<?php _e('分类：'); ?><?php $this->category(','); ?>
+    			</div>
+                <div class="post-content">
+        			<?php $this->content('阅读剩余部分...'); ?>
+                </div>
+    		</article>
+    	<?php endwhile; ?>
+        <?php else: ?>
+            <article class="post">
+                <h2 class="post-title"><?php _e('没有找到内容'); ?></h2>
+            </article>
+        <?php endif; ?>
 
-        <ol class="pages clearfix">
-            <?php $this->pageNav(); ?>
-        </ol>
-    </div><!-- end #content-->
+        <?php $this->pageNav(); ?>
+    </div><!-- end #main -->
+
 	<?php $this->need('sidebar.php'); ?>
 	<?php $this->need('footer.php'); ?>
--- usr/themes/replica/page.php
+++ usr/themes/replica/page.php
@@ -3,10 +3,6 @@
 <div class="col-mb-12 col-8" id="main">
     <article class="post">
 		<h2 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
-		<div class="post-meta">
-			<span><?php _e('作者：'); ?><?php $this->author(); ?></span>
-			<?php _e('发布时间：'); ?><?php $this->date('F j, Y'); ?>
-		</div>
         <div class="post-content">
     		<?php $this->content(); ?>
         </div>
