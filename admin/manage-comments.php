18f4ef364159ad569ee2e5f7874631d25a28d9a4
fen <fen1211@gmail.com>
2013-11-06 12:08:29 +0800

    issue #56 导航改进

--- admin/manage-comments.php
+++ admin/manage-comments.php
@@ -237,7 +237,7 @@ $(document).ready(function () {
         } else {
             var form = $('<form method="post" action="'
                 + t.attr('rel') + '" class="comment-reply">'
-                + '<p><textarea name="text" class="w-90" rows="3"></textarea></p>'
+                + '<p><label for="text" class="visuallyhidden"><?php _e('内容'); ?></label><textarea id="text" name="text" class="w-90" rows="3"></textarea></p>'
                 + '<p><button type="submit" class="btn-s primary"><?php _e('回复'); ?></button> <button type="button" class="btn-s cancel"><?php _e('取消'); ?></button></p>'
                 + '</form>').insertBefore($('.comment-action', td));
 
