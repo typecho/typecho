a914c2b6d370705dbf4343f969c33cf63f084ebf
fen <fen1211@gmail.com>
2013-12-01 12:55:17 +0800

    修复bug汇报链接

--- admin/copyright.php
+++ admin/copyright.php
@@ -6,7 +6,7 @@
     <nav class="resource">
         <a href="http://docs.typecho.org"><?php _e('帮助文档'); ?></a> &bull;
         <a href="http://forum.typecho.org"><?php _e('支持论坛'); ?></a> &bull;
-        <a href="https://github.com/typecho/typecho-replica/issues"><?php _e('报告错误'); ?></a> &bull;
+        <a href="https://github.com/typecho/typecho/issues"><?php _e('报告错误'); ?></a> &bull;
         <a href="http://extends.typecho.org"><?php _e('资源下载'); ?></a>
     </nav>
 </div>
