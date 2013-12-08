fc292e1c0aeebb306299cca403a7a3fc702db961
fen <fen1211@gmail.com>
2013-12-09 00:08:28 +0800

    tab 转 space

--- usr/themes/default/page.php
+++ usr/themes/default/page.php
@@ -2,12 +2,12 @@
 
 <div class="col-mb-12 col-8" id="main" role="main">
     <article class="post">
-		<h1 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h1>
+        <h1 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h1>
         <div class="post-content">
-    		<?php $this->content(); ?>
+            <?php $this->content(); ?>
         </div>
-	</article>
-	<?php $this->need('comments.php'); ?>
+    </article>
+    <?php $this->need('comments.php'); ?>
 </div><!-- end #main-->
 
 <?php $this->need('sidebar.php'); ?>
