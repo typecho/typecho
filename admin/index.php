9314ab0ce7793947f56259468988b338d74c4269
fen <fen1211@gmail.com>
2013-12-09 23:43:59 +0800

    修改后台首页，官方日志部分未修复

--- admin/index.php
+++ admin/index.php
@@ -9,10 +9,12 @@ $stat = Typecho_Widget::widget('Widget_Stat');
     <div class="container typecho-dashboard">
         <?php include 'page-title.php'; ?>
         <div class="colgroup typecho-page-main">
-            <div class="col-mb-12 col-tb-3 typecho-dashboard-nav" role="main">
-                <p class="intro"><?php _e('欢迎使用 Typecho, 您可以使用下面的链接开始您的 Blog 之旅:'); ?></p>
-            
-                <ul class="intro-link">
+            <div class="col-mb-12 welcome-board" role="main">
+                <?php _e('目前有 <em>%s</em> 篇日志, 并有 <em>%s</em> 条关于你的评论在 <em>%s</em> 个分类中.', 
+                $stat->myPublishedPostsNum, $stat->myPublishedCommentsNum, $stat->categoriesNum); ?>
+                <br><?php _e('使用下面的链接开始你的故事吧:'); ?>
+        
+                <ul id="start-link" class="clearfix">
                     <?php if($user->pass('contributor', true)): ?>
                     <li><a href="<?php $options->adminUrl('write-post.php'); ?>"><?php _e('撰写新文章'); ?></a></li>
                     <?php if($user->pass('editor', true) && 'on' == $request->get('__typecho_all_comments') && $stat->waitingCommentsNum > 0): ?> 
@@ -20,7 +22,7 @@ $stat = Typecho_Widget::widget('Widget_Stat');
                         <span class="balloon"><?php $stat->waitingCommentsNum(); ?></span>
                         </li>
                     <?php elseif($stat->myWaitingCommentsNum > 0): ?>
-                        <li><a href="<?php $options->adminUrl('manage-comments.php?status=waiting'); ?>"><?php _e('待审核的评论'); ?></a>
+                        <li><a href="<?php $options->adminUrl('manage-comments.php?status=waiting'); ?>"><?php _e('待审核评论'); ?></a>
                         <span class="balloon"><?php $stat->myWaitingCommentsNum(); ?></span>
                         </li>
                     <?php endif; ?>
@@ -34,36 +36,33 @@ $stat = Typecho_Widget::widget('Widget_Stat');
                         </li>
                     <?php endif; ?>
                     <?php if($user->pass('administrator', true)): ?>
-                    <li><a href="<?php $options->adminUrl('themes.php'); ?>"><?php _e('更换外观模板'); ?></a></li>
-                    <li><a href="<?php $options->adminUrl('options-general.php'); ?>"><?php _e('修改系统设置'); ?></a></li>
+                    <li><a href="<?php $options->adminUrl('themes.php'); ?>"><?php _e('更换外观'); ?></a></li>
+                    <li><a href="<?php $options->adminUrl('options-general.php'); ?>"><?php _e('系统设置'); ?></a></li>
                     <?php endif; ?>
                     <?php endif; ?>
                     <!--<li><a href="<?php $options->adminUrl('profile.php'); ?>"><?php _e('更新我的资料'); ?></a></li>-->
                 </ul>
-            
-                <h3><?php _e('统计信息'); ?></h3>
-                <div class="status">
-                    <p><?php _e('目前有 <em>%s</em> 篇 Blog, 并有 <em>%s</em> 条关于你的评论在已设定的 <em>%s</em> 个分类中.', 
-                    $stat->myPublishedPostsNum, $stat->myPublishedCommentsNum, $stat->categoriesNum); ?></p>
-                    
-                    <p><?php 
-                    if ($user->logged > 0) {
-                        _e('最后登录: %s', Typecho_I18n::dateWord($user->logged  + $options->timezone, $options->gmtTime + $options->timezone));
-                    }
-                    ?></p>
+                <?php $version = Typecho_Cookie::get('__typecho_check_version'); ?>
+                <?php if ($version && $version['available']): ?>
+                <div class="update-check">
+                    <p>
+                        <?php _e('您当前使用的版本是'); ?> <?php echo $version['current']; ?> &rarr;
+                        <strong><a href="<?php echo $version['link']; ?>"><?php _e('官方最新版本是'); ?> <?php echo $version['latest']; ?></a></strong>
+                    </p>
                 </div>
+                <?php endif; ?>
             </div>
 
-            <div class="col-mb-12 col-tb-6 typecho-dashboard-main" role="complementary">
-                <section>
-                    <h3><?php _e('最近发表的文章'); ?></h3>
-                    <?php Typecho_Widget::widget('Widget_Contents_Post_Recent', 'pageSize=7')->to($posts); ?>
+            <div class="col-mb-12 col-tb-4" role="complementary">
+                <section class="latest-link">
+                    <h3><?php _e('最近发布的文章'); ?></h3>
+                    <?php Typecho_Widget::widget('Widget_Contents_Post_Recent', 'pageSize=10')->to($posts); ?>
                     <ul>
                     <?php if($posts->have()): ?>
                     <?php while($posts->next()): ?>
                         <li>
+                            <span><?php $posts->date('m.d'); ?></span>
                             <a href="<?php $posts->permalink(); ?>" class="title"><?php $posts->title(); ?></a>
-                            <span>- <?php $posts->dateWord(); ?></span>
                         </li>
                     <?php endwhile; ?>
                     <?php else: ?>
@@ -71,41 +70,37 @@ $stat = Typecho_Widget::widget('Widget_Stat');
                     <?php endif; ?>
                     </ul>
                 </section>
+            </div>
 
-            	<section>
-                    <h3><?php _e('最新得到的回复'); ?></h3>
+            <div class="col-mb-12 col-tb-4" role="complementary">
+                <section class="latest-link">
+                    <h3><?php _e('最近得到的回复'); ?></h3>
                     <ul>
-                        <?php Typecho_Widget::widget('Widget_Comments_Recent', 'pageSize=7')->to($comments); ?>
+                        <?php Typecho_Widget::widget('Widget_Comments_Recent', 'pageSize=10')->to($comments); ?>
                         <?php if($comments->have()): ?>
                         <?php while($comments->next()): ?>
                         <li>
-                            <a href="<?php $comments->permalink(); ?>" class="title"><?php $comments->title(); ?></a>
-                            <span>- <?php $comments->dateWord(); ?>, <?php $comments->author(true); ?></span>
+                            <span><?php $comments->date('m.d'); ?></span>
+                            <a href="<?php $comments->permalink(); ?>" class="title"><?php $comments->author(true); ?></a>:
+                            <?php $comments->excerpt(35, '...'); ?>
                         </li>
                         <?php endwhile; ?>
                         <?php else: ?>
-                        <li><em><?php _e('暂时没有回复'); ?></em></li>
+                        <li><?php _e('暂时没有回复'); ?></li>
                         <?php endif; ?>
                     </ul>
                 </section>
             </div>
 
-            <div class="col-mb-12 col-tb-3 typecho-dashboard-nav" role="complementary">
-                <?php $version = Typecho_Cookie::get('__typecho_check_version'); ?>
-                <?php if ($version && $version['available']): ?>
-                <div class="update-check">
-                    <p>
-                        <?php _e('您当前使用的版本是'); ?> <?php echo $version['current']; ?><br>
-                        <strong><a href="<?php echo $version['link']; ?>"><?php _e('官方最新版本是'); ?> <?php echo $version['latest']; ?></a></strong>
-                    </p>
-                </div>
-                <?php endif; ?>
-                <h3><?php _e('官方消息'); ?></h3>
-                <div id="typecho-message" class="intro-link">
-                    <ul>
-                        <li><?php _e('读取中...'); ?></li>
-                    </ul>
-                </div>
+            <div class="col-mb-12 col-tb-4" role="complementary">
+                <section class="latest-link">
+                    <h3><?php _e('官方最新日志'); ?></h3>
+                    <div id="typecho-message">
+                        <ul>
+                            <li><?php _e('读取中...'); ?></li>
+                        </ul>
+                    </div>
+                </section>
             </div>
         </div>
     </div>
@@ -118,7 +113,7 @@ include 'common-js.php';
 
 <script>
 $(document).ready(function () {
-    var ul = $('.intro-link ul'), cache = window.sessionStorage,
+    var ul = $('#typecho-message ul'), cache = window.sessionStorage,
         html = cache ? cache.getItem('feed') : '',
         update = cache ? cache.getItem('update') : '';
 
@@ -129,8 +124,8 @@ $(document).ready(function () {
         $.get('<?php $options->index('/action/ajax?do=feed'); ?>', function (o) {
             for (var i = 0; i < o.length; i ++) {
                 var item = o[i];
-                html += '<li><a href="' + item.link + '" target="_blank">' + item.title 
-                    + '</a> <span>' + item.date + '</span></li>';
+                html += '<li><span>' + item.date + '</span> <a href="' + item.link + '" target="_blank">' + item.title 
+                    + '</a></li>';
             }
 
             ul.html(html);
@@ -144,7 +139,7 @@ $(document).ready(function () {
                 + '<?php _e('您当前使用的版本是 %s'); ?>'.replace('%s', update.current) + '<br />'
                 + '<strong><a href="' + update.link + '" target="_blank">' 
                 + '<?php _e('官方最新版本是 %s'); ?>'.replace('%s', update.latest) + '</a></strong></p></div>')
-            .prependTo('.typecho-dashboard-nav').effect('highlight');
+            .appendTo('.welcome-board').effect('highlight');
         }
     }
 
--- admin/menu.php
+++ admin/menu.php
@@ -5,7 +5,11 @@
     </nav>
     <div class="operate">
         <?php Typecho_Plugin::factory('admin/menu.php')->navBar(); ?>
-        <a href="<?php $options->adminUrl('profile.php'); ?>" class="author"><?php $user->screenName(); ?></a><a class="exit" href="<?php $options->logoutUrl(); ?>"><?php _e('登出'); ?></a><a href="<?php $options->siteUrl(); ?>">网站</a>
+        <a title="<?php 
+                    if ($user->logged > 0) {
+                        _e('最后登录: %s', Typecho_I18n::dateWord($user->logged  + $options->timezone, $options->gmtTime + $options->timezone));
+                    }
+                    ?>" href="<?php $options->adminUrl('profile.php'); ?>" class="author"><?php $user->screenName(); ?></a><a class="exit" href="<?php $options->logoutUrl(); ?>"><?php _e('登出'); ?></a><a href="<?php $options->siteUrl(); ?>">网站</a>
     </div>
 </div>
 
