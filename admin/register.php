5346434a2b861d3f1caada6b0eac3cd1ca42eb40
fen <fen1211@gmail.com>
2013-12-10 10:31:38 +0800

    改进后台日期显示 改进登录页面居中问题

--- admin/register.php
+++ admin/register.php
@@ -11,28 +11,29 @@ Typecho_Cookie::delete('__typecho_remember_mail');
 
 include 'header.php';
 ?>
-
-<div class="typecho-login">
-    <h1><a href="http://typecho.org" class="i-logo">Typecho</a></h1>
-    <form action="<?php $options->registerAction(); ?>" method="post" name="register" role="form">
-        <p>
-            <label for="name" class="sr-only"><?php _e('用户名'); ?></label>
-            <input type="text" id="name" name="name" placeholder="<?php _e('用户名'); ?>" value="<?php echo $rememberName; ?>" class="text-l w-100" />
+<div class="typecho-login-wrap">
+    <div class="typecho-login">
+        <h1><a href="http://typecho.org" class="i-logo">Typecho</a></h1>
+        <form action="<?php $options->registerAction(); ?>" method="post" name="register" role="form">
+            <p>
+                <label for="name" class="sr-only"><?php _e('用户名'); ?></label>
+                <input type="text" id="name" name="name" placeholder="<?php _e('用户名'); ?>" value="<?php echo $rememberName; ?>" class="text-l w-100" />
+            </p>
+            <p>
+                <label for="mail" class="sr-only"><?php _e('Email'); ?></label>
+                <input type="email" id="mail" name="mail" placeholder="<?php _e('Email'); ?>" value="<?php echo $rememberMail; ?>" class="text-l w-100" />
+            </p>
+            <p class="submit">
+                <button type="submit" class="btn-l w-100 primary"><?php _e('注册'); ?></button>
+            </p>
+        </form>
+        
+        <p class="more-link">
+            <a href="<?php $options->siteUrl(); ?>"><?php _e('返回首页'); ?></a>
+            &bull;
+            <a href="<?php $options->adminUrl('login.php'); ?>"><?php _e('用户登录'); ?></a>
         </p>
-        <p>
-            <label for="mail" class="sr-only"><?php _e('Email'); ?></label>
-            <input type="email" id="mail" name="mail" placeholder="<?php _e('Email'); ?>" value="<?php echo $rememberMail; ?>" class="text-l w-100" />
-        </p>
-        <p class="submit">
-            <button type="submit" class="btn-l w-100 primary"><?php _e('注册'); ?></button>
-        </p>
-    </form>
-    
-    <p class="more-link">
-        <a href="<?php $options->siteUrl(); ?>"><?php _e('返回首页'); ?></a>
-        &bull;
-        <a href="<?php $options->adminUrl('login.php'); ?>"><?php _e('用户登录'); ?></a>
-    </p>
+    </div>
 </div>
 <?php 
 include 'common-js.php';
@@ -40,6 +41,7 @@ include 'common-js.php';
 <script>
 $(document).ready(function () {
     $('#name').focus();
+    $('body').css('height', '100%');
 });
 </script>
 <?php
