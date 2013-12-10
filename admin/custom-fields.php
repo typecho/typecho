d02097dd3d09f6e86ad64db033eb1e837b913e29
joyqi <magike.net@gmail.com>
2013-12-10 12:41:42 +0800

    帮助文档链接修正

--- admin/custom-fields.php
+++ admin/custom-fields.php
@@ -68,6 +68,6 @@ $defaultFields = isset($post) ? $post->getDefaultFieldItems() : $page->getDefaul
                         </table>
                         <div class="description clearfix">
                             <button type="button" class="btn-xs operate-add"><?php _e('+添加字段'); ?></button>
-                            <?php _e('自定义字段可以扩展你的模板功能, 使用方法参见 <a href="">帮助文档</a>'); ?>
+                            <?php _e('自定义字段可以扩展你的模板功能, 使用方法参见 <a href="http://docs.typecho.org/help/custom-fields">帮助文档</a>'); ?>
                         </div>
                     </section>
