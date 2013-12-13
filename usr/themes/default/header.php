b15350075ce3fda48fafcdaff470d6f6e7c491df
fen <fen1211@gmail.com>
2013-12-13 17:09:44 +0800

    改进编辑器按钮

--- usr/themes/default/header.php
+++ usr/themes/default/header.php
@@ -1,8 +1,5 @@
 <!DOCTYPE HTML>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html class="no-js">
 <head>
     <meta charset="<?php $this->options->charset(); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
