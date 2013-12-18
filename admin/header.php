f4511ec67f4c4b4c9fc89208dccb4c767c881da5
fen <fen1211@gmail.com>
2013-12-18 17:45:31 +0800

    添加 dialog

--- admin/header.php
+++ admin/header.php
@@ -27,5 +27,5 @@ $header = Typecho_Plugin::factory('admin/header.php')->header($header);
     </head>
     <body<?php if (isset($bodyClass)) {echo ' class="' . $bodyClass . '"';} ?>>
     <!--[if lt IE 9]>
-        <div class="message error browsehappy"><?php _e('当前网页 <strong>不支持</strong> 你正在使用的浏览器. 为了正常的访问, 请 <a href="http://browsehappy.com/">升级你的浏览器</a>'); ?>.</div>
+        <div class="message error browsehappy" role="dialog"><?php _e('当前网页 <strong>不支持</strong> 你正在使用的浏览器. 为了正常的访问, 请 <a href="http://browsehappy.com/">升级你的浏览器</a>'); ?>.</div>
     <![endif]-->
--- usr/themes/default/header.php
+++ usr/themes/default/header.php
@@ -27,7 +27,7 @@
 </head>
 <body>
 <!--[if lt IE 8]>
-    <div class="browsehappy"><?php _e('当前网页 <strong>不支持</strong> 你正在使用的浏览器. 为了正常的访问, 请 <a href="http://browsehappy.com/">升级你的浏览器</a>'); ?>.</div>
+    <div class="browsehappy" role="dialog"><?php _e('当前网页 <strong>不支持</strong> 你正在使用的浏览器. 为了正常的访问, 请 <a href="http://browsehappy.com/">升级你的浏览器</a>'); ?>.</div>
 <![endif]-->
 
 <header id="header" class="clearfix">
