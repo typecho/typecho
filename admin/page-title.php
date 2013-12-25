785395167f8aadcfa0ab596d6b8084b6a5c6c4be
fen <fen1211@gmail.com>
2013-12-25 13:56:33 +0800

    改进栅格系统

--- admin/page-title.php
+++ admin/page-title.php
@@ -1,10 +1,8 @@
 <?php if(!defined('__TYPECHO_ADMIN__')) exit; ?>
-<div class="colgroup">
-    <div class="typecho-page-title col-mb-12">
-        <h2><?php echo $menu->title; ?><?php 
-        if (!empty($menu->addLink)) {
-            echo "<a href=\"{$menu->addLink}\">" . _t("新增") . "</a>";
-        }
-        ?></h2>
-    </div>
+<div class="typecho-page-title">
+    <h2><?php echo $menu->title; ?><?php 
+    if (!empty($menu->addLink)) {
+        echo "<a href=\"{$menu->addLink}\">" . _t("新增") . "</a>";
+    }
+    ?></h2>
 </div>
