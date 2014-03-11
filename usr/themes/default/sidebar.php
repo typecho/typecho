fa7bc750f072bb9b6a9d55e32d915f7fef65fae2
祁宁 <magike.net@gmail.com>
2014-03-11 14:45:21 +0800

    笔误。。。

--- usr/themes/default/sidebar.php
+++ usr/themes/default/sidebar.php
@@ -25,7 +25,7 @@
     <?php if (!empty($this->options->sidebarBlock) && in_array('ShowCategory', $this->options->sidebarBlock)): ?>
     <section class="widget">
 		<h3 class="widget-title"><?php _e('分类'); ?></h3>
-        <?php $this->widget('Widget_Metas_Category_List')->listCategoies('wrapClass=widget-list'); ?>
+        <?php $this->widget('Widget_Metas_Category_List')->listCategories('wrapClass=widget-list'); ?>
 	</section>
     <?php endif; ?>
 
