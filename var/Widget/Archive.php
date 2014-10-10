1b4da70ee7385bc7b2f0526c98c2847f08fabe58
祁宁 <magike.net@gmail.com>
2014-10-10 15:48:45 +0800

    当某些情况下（比如升级）误删了皮肤目录，自动使用default皮肤

--- var/Widget/Archive.php
+++ var/Widget/Archive.php
@@ -234,7 +234,10 @@ class Widget_Archive extends Widget_Abstract_Contents
         }
         
         /** 初始化皮肤路径 */
-        $this->_themeDir = rtrim($this->options->themeFile($this->options->theme), '/') . '/';
+        $this->_themeDir = 
+            rtrim($this->options->themeFile(
+                is_dir($this->options->themeFile($this->options->theme)) ?
+                    $this->options->theme : 'default'), '/') . '/';
 
         /** 处理feed模式 **/
         if ('feed' == $this->parameter->type) {
