cc622f9ccc5fd6d5e2ab557c34e29a9be298f9ad
unknown <48353088@qq.com>
2015-10-31 15:33:46 +0800

    设置搜索归档缩略名

--- var/Widget/Archive.php
+++ var/Widget/Archive.php
@@ -1212,6 +1212,9 @@ class Widget_Archive extends Widget_Abstract_Contents
         /** 设置归档类型 */
         $this->_archiveType = 'search';
 
+        /** 设置归档缩略名 */
+        $this->_archiveSlug = $keywords;
+
         /** 插件接口 */
         $this->pluginHandle()->searchHandle($this, $select);
     }
