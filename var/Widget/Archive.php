c58a9530b16f60a917113b08dc3decafc1655618
joyqi <magike.net@gmail.com>
2016-01-25 16:58:27 +0800

    fix #416

--- var/Widget/Archive.php
+++ var/Widget/Archive.php
@@ -1369,7 +1369,7 @@ class Widget_Archive extends Widget_Abstract_Contents
         
         /** 初始化皮肤函数 */
         $functionsFile = $this->_themeDir . 'functions.php';
-        if (!$this->_invokeFromOutside && file_exists($functionsFile)) {
+        if ((!$this->_invokeFromOutside || $this->parameter->type == 404) && file_exists($functionsFile)) {
             require_once $functionsFile;
             if (function_exists('themeInit')) {
                 themeInit($this);
