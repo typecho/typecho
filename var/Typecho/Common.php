ac1c62a07d17cb27d7202113942b24d089e924ab
joyqi <magike.net@gmail.com>
2017-10-30 02:57:57 +0800

    new release

--- var/Typecho/Common.php
+++ var/Typecho/Common.php
@@ -22,7 +22,7 @@ define('__TYPECHO_MB_SUPPORTED__', function_exists('mb_get_info') && function_ex
 class Typecho_Common
 {
     /** 程序版本 */
-    const VERSION = '1.1/17.10.29';
+    const VERSION = '1.1/17.10.30';
 
     /**
      * 允许的属性
