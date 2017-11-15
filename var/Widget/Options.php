94881996b4d1b7ba810c5745fa4bc8ebad461133
joyqi <magike.net@gmail.com>
2017-11-15 17:51:29 +0800

    update plupload

--- var/Widget/Options.php
+++ var/Widget/Options.php
@@ -340,7 +340,7 @@ class Widget_Options extends Typecho_Widget
                 array('gif,jpg,jpeg,png,tiff,bmp', 'mp3,wmv,wma,rmvb,rm,avi,flv',
                     'txt,doc,docx,xls,xlsx,ppt,pptx,zip,rar,pdf'), $this->attachmentTypes);
             
-            $attachmentTypesResult = array_unique(array_map('trim', explode(',', $attachmentTypes)));
+            $attachmentTypesResult = array_unique(array_map('trim', preg_split("/(,|\.)/", $attachmentTypes)));
         }
         
         return $attachmentTypesResult;
