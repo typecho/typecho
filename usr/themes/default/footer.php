549971eaf982d04b1d741113f6bef1647820ba0b
海马 <1851963629@qq.com>
2019-02-05 15:54:27 +0800

    Update footer.php
    Modify the default template footer

--- usr/themes/default/footer.php
+++ usr/themes/default/footer.php
@@ -7,6 +7,7 @@
 <footer id="footer" role="contentinfo">
     &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
     <?php _e('由 <a href="http://www.typecho.org">Typecho</a> 强力驱动'); ?>.
+    <?php _e('本站点使用 <a href="https://haimablog.ooo">海马博客</a> 制作的针对TLS和PHP7专门优化的特殊版本'); ?>.
 </footer><!-- end #footer -->
 
 <?php $this->footer(); ?>
