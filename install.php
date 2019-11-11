9a9ad301c34735e5a187f6792463c8118eda3a74
KyleBing <kylebing@163.com>
2019-11-11 17:37:35 +0800

    修正 install 页面移动端显示问题，并重新整理 scss 结构

--- install.php
+++ install.php
@@ -176,6 +176,7 @@ Typecho_Cookie::set('__typecho_lang', $lang);
 <head lang="zh-CN">
     <meta charset="<?php _e('UTF-8'); ?>" />
 	<title><?php _e('Typecho 安装程序'); ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="admin/css/normalize.css" />
     <link rel="stylesheet" type="text/css" href="admin/css/grid.css" />
     <link rel="stylesheet" type="text/css" href="admin/css/style.css" />
