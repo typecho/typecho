b7f9fcba187e0278a534bb3c0a5a119f0ba46f0f
sy-records <52o@qq52o.cn>
2022-02-06 10:40:48 +0800

    Fix broken extends.typecho.org link
    close #857

--- admin/copyright.php
+++ admin/copyright.php
@@ -8,6 +8,6 @@
         <a href="http://docs.typecho.org"><?php _e('帮助文档'); ?></a> &bull;
         <a href="http://forum.typecho.org"><?php _e('支持论坛'); ?></a> &bull;
         <a href="https://github.com/typecho/typecho/issues"><?php _e('报告错误'); ?></a> &bull;
-        <a href="http://extends.typecho.org"><?php _e('资源下载'); ?></a>
+        <a href="http://typecho.org/download"><?php _e('资源下载'); ?></a>
     </nav>
 </div>
