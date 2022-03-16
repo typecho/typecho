2531ea3245ccadfb14e83512bb184a47661c6a46
我愿人长久 <ihewro@163.com>
2022-03-16 19:32:20 +0800

    fix: 文章feed中摘要字段的内容问题

--- var/Widget/Archive.php
+++ var/Widget/Archive.php
@@ -1498,7 +1498,7 @@ class Archive extends Contents
                     'date'            => $this->created,
                     'link'            => $this->permalink,
                     'author'          => $this->author,
-                    'excerpt'         => $this->description,
+                    'excerpt'         => $this->excerpt,
                     'comments'        => $this->commentsNum,
                     'commentsFeedUrl' => $feedUrl,
                     'suffix'          => $suffix
