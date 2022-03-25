6100695d87eb3c9336e1074d30fd900a88aafb7c
joyqi <joyqi@segmentfault.com>
2022-03-25 15:31:57 +0800

    fix db exception

--- var/Typecho/Common.php
+++ var/Typecho/Common.php
@@ -230,10 +230,10 @@ namespace Typecho {
                 //覆盖原始错误信息
                 $message = 'Database Server Error';
 
-                if ($exception instanceof \Typecho\Db\Adapter\SQLException) {
+                if ($exception instanceof \Typecho\Db\Adapter\ConnectionException) {
                     $code = 503;
                     $message = 'Error establishing a database connection';
-                } elseif ($exception instanceof \Typecho\Db\Query\Exception) {
+                } elseif ($exception instanceof \Typecho\Db\Adapter\SQLException) {
                     $message = 'Database Query Error';
                 }
             }
