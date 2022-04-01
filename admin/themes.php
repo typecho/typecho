5c0dd59de1a8c858bacf462055ac3e6bc03746a4
joyqi <joyqi@users.noreply.github.com>
2022-04-01 16:31:44 +0800

    Simpler missing theme judgement

--- admin/themes.php
+++ admin/themes.php
@@ -9,25 +9,10 @@ include 'menu.php';
         <?php include 'page-title.php'; ?>
         <div class="row typecho-page-main" role="main">
             <div class="col-mb-12">
-                <?php if(!is_dir($options->themeFile($options->usingTheme))): ?>
-                    <div id="typecho-welcome" class="message">
-                        <form action="<?php echo $security->getTokenUrl($security->getIndex("/action/themes-edit?change=default")); ?>" method="post">
-                            <h3><?php _e('检测到您之前使用的 "%s" 外观文件不存在: ', $options->usingTheme); ?></h3>
-                            <ul>
-                                <li><?php _e('您可以切换为<strong>默认外观</strong>，或者<strong>重新上传</strong>之前的外观文件后<strong>刷新</strong>本页面'); ?></li>
-                                <li><strong class="warning"><?php _e('切换为默认外观后，您之前的外观设置将被移除'); ?></strong></li>
-                            </ul>
-                            <p>
-                                <button type="submit" class="btn btn-warn"><?php _e('使用默认外观'); ?></button>
-                                <button type="button" class="btn primary" onclick="window.location.reload();"><?php _e('刷新'); ?></button>
-                            </p>
-                        </form>
-                    </div>
-                <?php else: ?>
                 <ul class="typecho-option-tabs fix-tabs clearfix">
                     <li class="current"><a href="<?php $options->adminUrl('themes.php'); ?>"><?php _e('可以使用的外观'); ?></a>
                     </li>
-                    <?php if (!defined('__TYPECHO_THEME_WRITEABLE__') || __TYPECHO_THEME_WRITEABLE__): ?>
+                    <?php if (\Widget\Themes\Files::isWriteable()): ?>
                         <li><a href="<?php $options->adminUrl('theme-editor.php'); ?>"><?php _e('编辑当前外观'); ?></a></li>
                     <?php endif; ?>
                     <?php if (\Widget\Themes\Config::isExists()): ?>
@@ -48,10 +33,21 @@ include 'menu.php';
                         </thead>
 
                         <tbody>
+                        <?php if ($options->missingTheme): ?>
+                            <tr id="theme-<?php $options->missingTheme; ?>" class="current">
+                                <td colspan="2" class="warning">
+                                    <p><strong><?php _e('检测到您之前使用的 "%s" 外观文件不存在，您可以重新上传此外观或者启用其他外观。', $options->missingTheme); ?></strong></p>
+                                    <ul>
+                                        <li><?php _e('重新上传此外观后刷新当前页面，此提示将会消失。'); ?></li>
+                                        <li><?php _e('启用新外观后，当前外观的设置数据将被删除。'); ?></li>
+                                    </ul>
+                                </td>
+                            </tr>
+                        <?php endif; ?>
                         <?php \Widget\Themes\Rows::alloc()->to($themes); ?>
                         <?php while ($themes->next()): ?>
                             <tr id="theme-<?php $themes->name(); ?>"
-                                class="<?php if ($themes->activated): ?>current<?php endif; ?>">
+                                class="<?php if ($themes->activated && !$options->missingTheme): ?>current<?php endif; ?>">
                                 <td valign="top"><img src="<?php $themes->screen(); ?>"
                                                       alt="<?php $themes->name(); ?>"/></td>
                                 <td valign="top">
@@ -61,9 +57,9 @@ include 'menu.php';
                                         <?php if ($themes->version): ?><?php _e('版本'); ?>: <?php $themes->version() ?><?php endif; ?>
                                     </cite>
                                     <p><?php echo nl2br($themes->description); ?></p>
-                                    <?php if ($options->theme != $themes->name): ?>
+                                    <?php if ($options->theme != $themes->name || $options->missingTheme): ?>
                                         <p>
-                                            <?php if (!defined('__TYPECHO_THEME_WRITEABLE__') || __TYPECHO_THEME_WRITEABLE__): ?>
+                                            <?php if (\Widget\Themes\Files::isWriteable()): ?>
                                                 <a class="edit"
                                                    href="<?php $options->adminUrl('theme-editor.php?theme=' . $themes->name); ?>"><?php _e('编辑'); ?></a> &nbsp;
                                             <?php endif; ?>
@@ -77,7 +73,6 @@ include 'menu.php';
                         </tbody>
                     </table>
                 </div>
-                <?php endif; ?>
             </div>
         </div>
     </div>
@@ -86,12 +81,5 @@ include 'menu.php';
 <?php
 include 'copyright.php';
 include 'common-js.php';
+include 'footer.php';
 ?>
-<script>
-    $('#typecho-welcome form').submit(function (e) {
-        if (!confirm('<?php _e('使用默认外观将移除之前的外观设置，是否继续?'); ?>')) {
-            return false;
-        }
-    });
-</script>
-<?php include 'footer.php'; ?>
