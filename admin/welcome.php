f7b84506f2955c3aa6b3c4e2bcf9bfb342c0c6a7
sy-records <52o@qq52o.cn>
2022-04-06 17:37:26 +0800

    Update admin welcome tip

--- admin/welcome.php
+++ admin/welcome.php
@@ -16,9 +16,9 @@ include 'menu.php';
                         <li><a class="operate-delete" href="<?php $options->adminUrl('profile.php#change-password'); ?>"><?php _e('强烈建议更改你的默认密码'); ?></a></li>
                         <?php if($user->pass('contributor', true)): ?>
                         <li><a href="<?php $options->adminUrl('write-post.php'); ?>"><?php _e('撰写第一篇日志'); ?></a></li>
-                        <li><a href="<?php $options->siteUrl(); ?>"><?php _e('查看我的站点'); ?></a></li>
+                        <li><a href="<?php $options->siteUrl(); ?>"><?php $user->pass('administrator', true) ? _e('查看我的站点') : _e('查看 "%s" 网站', $options->title); ?></a></li>
                         <?php else: ?>
-                        <li><a href="<?php $options->siteUrl(); ?>"><?php _e('查看我的站点'); ?></a></li>
+                        <li><a href="<?php $options->siteUrl(); ?>"><?php _e('查看 "%s" 网站', $options->title); ?></a></li>
                         <?php endif; ?>
                     </ol>
                     <p><button type="submit" class="btn primary"><?php _e('让我直接开始使用吧 &raquo;'); ?></button></p>
