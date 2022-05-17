8aedbfed952c00af1cc67a45ed3310e66b6d8d5b
sy-records <52o@qq52o.cn>
2022-05-17 09:37:37 +0800

    Fix call themeUrl error

--- usr/themes/default/header.php
+++ usr/themes/default/header.php
@@ -13,9 +13,9 @@
         ], '', ' - '); ?><?php $this->options->title(); ?></title>
 
     <!-- 使用url函数转换相关路径 -->
-    <link rel="stylesheet" href="<?php $this->options->themeUrl('normalize.css'); ?>">
-    <link rel="stylesheet" href="<?php $this->options->themeUrl('grid.css'); ?>">
-    <link rel="stylesheet" href="<?php $this->options->themeUrl('style.css'); ?>">
+    <link rel="stylesheet" href="<?php $this->options->themeUrl('normalize.css', null); ?>">
+    <link rel="stylesheet" href="<?php $this->options->themeUrl('grid.css', null); ?>">
+    <link rel="stylesheet" href="<?php $this->options->themeUrl('style.css', null); ?>">
 
     <!-- 通过自有函数输出HTML头部信息 -->
     <?php $this->header(); ?>
