afe428d37ed98c65f122229e68dfa6aa794f1f6c
SilentJMA <kilwanitro@gmail.com>
2023-09-23 12:23:25 +0200

    Update login.php
    Use require Instead of include: If these files are essential for your application to function correctly, consider using require instead of include. This way, if any of these files are missing, it will result in a fatal error, which can help you catch issues during development.
    
    Simplify HTML Markup: Use cleaner HTML markup with minimal classes and labels. This makes the code more straightforward.
    
    Use PHP Short Tags: You can use PHP short tags <?= ?> for echoing variables, which makes the code more concise.
    
    Avoid Mixing PHP and HTML: Try to separate PHP logic from HTML content. You can use a templating engine like Twig for more complex templates.
    
    Indentation and Consistency: Ensure consistent indentation and formatting throughout the code.

--- admin/login.php
+++ admin/login.php
@@ -1,56 +1,53 @@
 <?php
-include 'common.php';
+require 'common.php';
 
 if ($user->hasLogin()) {
     $response->redirect($options->adminUrl);
 }
+
 $rememberName = htmlspecialchars(\Typecho\Cookie::get('__typecho_remember_name', ''));
 \Typecho\Cookie::delete('__typecho_remember_name');
 
-$bodyClass = 'body-100';
-
 include 'header.php';
 ?>
+
 <div class="typecho-login-wrap">
     <div class="typecho-login">
         <h1><a href="https://typecho.org" class="i-logo">Typecho</a></h1>
-        <form action="<?php $options->loginAction(); ?>" method="post" name="login" role="form">
+        <form action="<?= $options->loginAction(); ?>" method="post" name="login" role="form">
             <p>
-                <label for="name" class="sr-only"><?php _e('用户名'); ?></label>
-                <input type="text" id="name" name="name" value="<?php echo $rememberName; ?>" placeholder="<?php _e('用户名'); ?>" class="text-l w-100" autofocus />
+                <input type="text" id="name" name="name" value="<?= $rememberName; ?>" placeholder="<?= _e('用户名'); ?>" class="text-l w-100" autofocus />
             </p>
             <p>
-                <label for="password" class="sr-only"><?php _e('密码'); ?></label>
-                <input type="password" id="password" name="password" class="text-l w-100" placeholder="<?php _e('密码'); ?>" />
+                <input type="password" id="password" name="password" class="text-l w-100" placeholder="<?= _e('密码'); ?>" />
             </p>
             <p class="submit">
-                <button type="submit" class="btn btn-l w-100 primary"><?php _e('登录'); ?></button>
-                <input type="hidden" name="referer" value="<?php echo $request->filter('html')->get('referer'); ?>" />
+                <button type="submit" class="btn btn-l w-100 primary"><?= _e('登录'); ?></button>
+                <input type="hidden" name="referer" value="<?= $request->filter('html')->get('referer'); ?>" />
             </p>
             <p>
                 <label for="remember">
-                    <input<?php if(\Typecho\Cookie::get('__typecho_remember_remember')): ?> checked<?php endif; ?> type="checkbox" name="remember" class="checkbox" value="1" id="remember" /> <?php _e('下次自动登录'); ?>
+                    <input<?= \Typecho\Cookie::get('__typecho_remember_remember') ? ' checked' : ''; ?> type="checkbox" name="remember" class="checkbox" value="1" id="remember" /> <?= _e('下次自动登录'); ?>
                 </label>
             </p>
         </form>
         
         <p class="more-link">
-            <a href="<?php $options->siteUrl(); ?>"><?php _e('返回首页'); ?></a>
-            <?php if($options->allowRegister): ?>
-            &bull;
-            <a href="<?php $options->registerUrl(); ?>"><?php _e('用户注册'); ?></a>
+            <a href="<?= $options->siteUrl(); ?>"><?= _e('返回首页'); ?></a>
+            <?php if ($options->allowRegister): ?>
+                &bull;
+                <a href="<?= $options->registerUrl(); ?>"><?= _e('用户注册'); ?></a>
             <?php endif; ?>
         </p>
     </div>
 </div>
-<?php 
-include 'common-js.php';
-?>
+
+<?php require 'common-js.php'; ?>
+
 <script>
-$(document).ready(function () {
-    $('#name').focus();
-});
+    $(document).ready(function () {
+        $('#name').focus();
+    });
 </script>
-<?php
-include 'footer.php';
-?>
+
+<?php include 'footer.php'; ?>
