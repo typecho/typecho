c816efa26eb6e2e7b368f4e1e4c9975f70fb8738
Lu Fei <52o@qq52o.cn>
2024-01-09 14:56:25 +0800

    fix trim error (#1697)

--- var/Widget/Contents/Post/Edit.php
+++ var/Widget/Contents/Post/Edit.php
@@ -75,7 +75,7 @@ class Edit extends Contents implements ActionInterface
 
             /** 发送ping */
             $trackback = array_filter(
-                array_unique(preg_split("/(\r|\n|\r\n)/", trim($this->request->get('trackback'))))
+                array_unique(preg_split("/(\r|\n|\r\n)/", trim($this->request->get('trackback', ''))))
             );
             Service::alloc()->sendPing($this, $trackback);
 
