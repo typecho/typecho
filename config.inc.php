4c513f51255555556cb9a481df8f55d136de3da2
aw168 <965868345@qq.com>
2025-04-02 20:15:39 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -36,12 +36,12 @@ Typecho_Common::init();
 /** 定义数据库参数 */
 $db = new Typecho_Db('Pdo_Mysql', 'typecho_');
 $db->addServer(array (
-  'host' => '数据库地址',
-  'user' => '数据库用户名',
-  'password' => '数据库密码',
+  'host' => 'sql105.infinityfree.com',
+  'user' => 'if0_38468020',
+  'password' => 'piBtlGAImShS',
   'charset' => 'utf8mb4',
   'port' => '3306',
-  'database' => '数据库昵称',
+  'database' => 'if0_38468020_vve',
   'engine' => 'MyISAM',
 ), Typecho_Db::READ | Typecho_Db::WRITE);
 Typecho_Db::set($db);
