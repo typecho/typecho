1fd850ff8acbd032ec8808a97da705a71517be33
jrotty <bssf@qq.com>
2026-01-24 23:01:41 +0800

    首次发布文章时不自动勾选默认分类
    首次发文章时不需要程序自动勾选默认分类

--- admin/write-post.php
+++ admin/write-post.php
@@ -97,7 +97,13 @@ $post = \Widget\Contents\Post\Edit::alloc()->prepare();
                         <label class="typecho-label"><?php _e('分类'); ?></label>
                         <?php \Widget\Metas\Category\Rows::alloc()->to($category); ?>
                         <ul>
-                            <?php $categories = array_column($post->categories, 'mid'); ?>
+                            <?php 
+                            if ($post->have()) {
+                                $categories = array_column($post->categories, 'mid');
+                            } else {
+                                $categories = [];
+                            }
+                            ?>
                             <?php while ($category->next()): ?>
                                 <li><?php echo str_repeat('&nbsp;&nbsp;&nbsp;&nbsp;', $category->levels); ?><input
                                         type="checkbox" id="category-<?php $category->mid(); ?>"
